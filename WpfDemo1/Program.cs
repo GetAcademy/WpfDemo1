eebd7cd849f856cb288d888f64d1dbe99c5d99ff
GET Academy <terje@getacademy.no>
2022-11-01 11:35:07 +0100

    Initial commit

---
+++ WpfDemo1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDemo1
+{
+    internal class Program
+    {
+        private static Label label;
+        private static int count = 0;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            window.Title = "Hallo verden!";
+
+
+            var button = new Button
+            {
+                Content = "Trykk på meg!"
+            };
+            label = new Label();
+            button.Click += ButtonClick;
+
+            var stackPanel = new StackPanel();
+            stackPanel.Children.Add(label);
+            stackPanel.Children.Add(button);
+
+
+            window.Content = stackPanel;
+
+            app.Run(window);
+        }
+
+        private static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            count++;
+            label.Content = count;
+        }
+    }
+}
